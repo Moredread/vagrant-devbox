7bddf667654a0fe574dfcccc6845ad67c2418f37
Andre-Patrick Bubel <code@andre-bubel.de>
2015-10-24 00:21:45 +0200

    Add firefox and chromium

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 pacman -Suy --noconfirm
 pacman -R --noconfirm virtualbox-guest-utils-nox
-pacman -S --noconfirm --needed less zsh i3 gdm dmenu git tk gnome-keyring python2 python2-pip python-pip xorg-server-utils xorg-apps virtualbox-guest-utils gvim rxvt-unicode
+pacman -S --noconfirm --needed less zsh i3 gdm dmenu git tk gnome-keyring python2 python2-pip python-pip xorg-server-utils xorg-apps virtualbox-guest-utils gvim rxvt-unicode firefox chromium
 
 cat <<EOF >/home/vagrant/.autostart.sh
 #!/bin/sh
