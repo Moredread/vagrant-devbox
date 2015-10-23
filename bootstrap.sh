56e6f0fbf8c3611c01a93b31f229c2e4754bb901
Andre-Patrick Bubel <code@andre-bubel.de>
2015-10-24 00:04:03 +0200

    More stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,33 @@
 #!/bin/sh
 
 pacman -Suy --noconfirm
-pacman -S --noconfirm less zsh i3 gdm dmenu
+pacman -R --noconfirm virtualbox-guest-utils-nox
+pacman -S --noconfirm --needed less zsh i3 gdm dmenu git tk gnome-keyring python2 python2-pip python-pip xorg-server-utils xorg-apps virtualbox-guest-utils gvim rxvt-unicode
+
+cat <<EOF >/home/vagrant/.autostart.sh
+#!/bin/sh
+
+sleep 10
+/usr/bin/VBoxClient-all
+EOF
+chmod 755 /home/vagrant/.autostart.sh
+chown vagrant:vagrant /home/vagrant/.autostart.sh
+
+mkdir -p /home/vagrant/.config/autostart
+cat <<EOF >/home/vagrant/.config/autostart/xinitrc.desktop
+[Desktop Entry]
+Name=Autostart
+Comment=Automatically start listed applications when gdm starts.
+Exec=.autostart.sh
+Icon=/usr/share/icons/5.png
+Terminal=false
+Type=Application
+Categories=Configuration
+EOF
+chmod 755 /home/vagrant/.config/autostart
+chown vagrant:vagrant /home/vagrant/.config -R
+
+systemctl enable gdm
+systemctl start gdm
+systemctl enable vboxservice
+systemctl start vboxservice
