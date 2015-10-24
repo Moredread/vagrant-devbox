42c92619eb61a7a8f87879785fc3804c1b0cadfc
Andre-Patrick Bubel <code@andre-bubel.de>
2015-10-25 01:18:15 +0200

    Install bitstream Vera Mono

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ USERHOME=/home/vagrant
 
 pacman -Suy --noconfirm
 pacman -R --noconfirm virtualbox-guest-utils-nox
-pacman -S --noconfirm --needed less zsh i3 gdm dmenu git tk gnome-keyring python2 python2-pip python-pip xorg-server-utils xorg-apps virtualbox-guest-utils gvim rxvt-unicode firefox chromium xclip
+pacman -S --noconfirm --needed less zsh i3 gdm dmenu git tk gnome-keyring python2 python2-pip python-pip xorg-server-utils xorg-apps virtualbox-guest-utils gvim rxvt-unicode firefox chromium xclip ttf-bitstream-vera
 
 chsh -s /bin/zsh vagrant
 if [ ! -d "${USERHOME}/.oh-my-zsh" ]; then
