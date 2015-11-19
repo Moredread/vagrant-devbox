b78b13eac7845753ada0cf9e712ea47688375dcd
Andre-Patrick Bubel <code@andre-bubel.de>
2015-11-19 18:32:42 +0100

    Install more haskell packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ USERHOME=/home/vagrant
 pacman -Suy --noconfirm
 pacman -R --noconfirm virtualbox-guest-utils-nox
 pacman -S --noconfirm --needed less zsh i3 gdm dmenu git tk gnome-keyring python2 python2-pip python-pip xorg-server-utils xorg-apps virtualbox-guest-utils gvim rxvt-unicode firefox chromium xclip ttf-bitstream-vera mesa-demos qt4 xsel
-pacman -S --noconfirm --needed wget docker lxc btrfs-progs lua-filesystem lua-alt-getopt ghc cabal-install
+pacman -S --noconfirm --needed wget docker lxc btrfs-progs lua-filesystem lua-alt-getopt ghc cabal-install happy haddock alex
 
 chsh -s /bin/zsh vagrant
 if [ ! -d "${USERHOME}/.oh-my-zsh" ]; then
