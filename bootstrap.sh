327905d380d34e29eb492086d086d0c8f70ab4a0
Andre-Patrick Bubel <code@andre-bubel.de>
2015-12-20 16:28:59 +0100

    Add more packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ USERHOME=/home/vagrant
 pacman -Suy --noconfirm
 pacman -R --noconfirm virtualbox-guest-utils-nox
 pacman -S --noconfirm --needed less zsh i3 gdm dmenu git tk gnome-keyring python2 python2-pip python-pip xorg-server-utils xorg-apps virtualbox-guest-utils gvim rxvt-unicode firefox chromium xclip ttf-bitstream-vera mesa-demos qt4 xsel
-pacman -S --noconfirm --needed wget docker lxc btrfs-progs lua-filesystem lua-alt-getopt ghc cabal-install happy haddock alex
+pacman -S --noconfirm --needed wget docker lxc btrfs-progs lua-filesystem lua-alt-getopt ghc cabal-install happy haddock alex ccache cifs-utils atop sysstat
 
 chsh -s /bin/zsh vagrant
 if [ ! -d "${USERHOME}/.oh-my-zsh" ]; then
